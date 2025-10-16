2877134ee5fbbdb9dcce11ae4e82347741852e73
Manan Shori <42857508+mnnshori@users.noreply.github.com>
2025-10-16 18:50:13 +0530

    Enhance linked list with error handling and cleanup
    Added error handling for input and memory allocation failures. Implemented a function to free the linked list.

--- Problem Solving/Data Structures/Linked Lists/Insert a Node at the Tail of a Linked List/InsertNodeTailRecursive.c
+++ Problem Solving/Data Structures/Linked Lists/Insert a Node at the Tail of a Linked List/InsertNodeTailRecursive.c
@@ -17,26 +17,48 @@ struct LinkedList* createLinkedList();
 void printList(struct Node* traverse);
 void insertNodeTail(struct LinkedList* myList, int data);
 void recursiveInsert(struct Node* traverse, int data);
+void freeListRecursive(struct Node* ptr);
+void deleteList(struct LinkedList* myList);
 
 int main() {
     struct LinkedList* myList = createLinkedList();
     int n;
     printf("input n: ");
-    scanf("%d", &n);    
+        
+
+    if(scanf("%d", &n) != 1) {
+        fprintf(stderr, "Error reading number of elements.\n");
+        free(myList);
+        return 1;
+    }
 
     printf("input %d numbers: ", n);
     for(int i=0; i<n; i++) {
         int temp;
-        scanf("%d", &temp);
+        
+
+        if( scanf("%d", &temp ) != 1) {
+            fprintf(stderr, "Error reading element.\n");
+            deleteList(myList);
+            return 1;
+        }
 
         insertNodeTail(myList, temp);
     }
     printList((*myList).listHead);
+
+    deleteList(myList);
     return 0;
 }
 
 struct Node* createNode(int data) {
     struct Node* myNode = (struct Node*)realloc(NULL, 1 * sizeof(struct Node));
+
+    if(myNode == NULL) {
+        perror("Failed to allocate memory for Node");
+        exit(EXIT_FAILURE);
+    }
+
     (*myNode).data = data;
     (*myNode).next = NULL;
     return myNode;
@@ -44,6 +66,12 @@ struct Node* createNode(int data) {
 
 struct LinkedList* createLinkedList() {
     struct LinkedList* myList = (struct LinkedList*)realloc(NULL, 1 * sizeof(struct LinkedList));
+
+    if(myList == NULL) {
+        perror("Failed to allocate memory for struct LinkedList");
+        exit(EXIT_FAILURE);
+    }
+
     (*myList).listHead = NULL;
     return myList;
 }
@@ -78,4 +106,26 @@ void recursiveInsert(struct Node* traverse, int data) {
         return;
     }
     recursiveInsert((*traverse).next, data);
+}
+
+void freeListRecursive(struct Node* ptr) {
+    if(ptr == NULL) {
+        return;
+    }
+
+    printf( "reached %d...\n", (*ptr).data );
+    freeListRecursive( (*ptr).next );
+    
+    printf( "freeing up %d...\n", (*ptr).data );
+    free(ptr);
+}
+
+void deleteList(struct LinkedList* myList) {
+    if( myList == NULL) {
+        return;
+    }
+    freeListRecursive((*myList).listHead);
+    
+    (*myList).listHead = NULL;
+    free(myList);
 }
