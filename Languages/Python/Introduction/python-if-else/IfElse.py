d74e4c0dd1dd662cd32852516d516329a0e905fd
Manan Shori <42857508+mnnshori@users.noreply.github.com>
2026-02-19 18:19:14 +0530

    Added IfElse.py
    Note that else at the end is better than elif at the end of an if-elif-else block.

---
+++ Languages/Python/Introduction/python-if-else/IfElse.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    if n&1:
+        print("Weird")
+    elif 2 <= n <= 5:
+        print("Not Weird")
+    elif 6 <= n <= 20:
+        print("Weird") 
+    else:
+        print("Not Weird") 
